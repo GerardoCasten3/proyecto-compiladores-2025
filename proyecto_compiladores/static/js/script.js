9e96b72e4e0bcdbb67a0e4ba30468e53b97b19fc
Aguirre120503 <100791122+Aguirre120503@users.noreply.github.com>
2025-10-06 19:18:04 -0600

    conexion con front

--- proyecto_compiladores/static/js/script.js
+++ proyecto_compiladores/static/js/script.js
@@ -1,10 +1,29 @@
 const boton = document.getElementById('btnEnviar');
 
 boton.addEventListener('click', function() {
-    Swal.fire({
-    title: "Exito",
-    icon: "success",
-    text: 'AQUI VA EL MENSAJE DE EXITO',
-    draggable: true
-    });
+    const textoEntrada = document.getElementById('textoEntrada').value;
+    const url = '/analizar'; 
+
+    fetch(url, {
+
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ source_code: textoEntrada })
+    }).then(response => response.json())
+      .then(data => {
+          const tablaResultados = document.getElementById('tablaResultados').getElementsByTagName('tbody')[0];
+          tablaResultados.innerHTML = ''; // Limpiar la tabla antes de llenarla con nuevos datos
+          data.tokens.forEach(token => {
+              const fila = tablaResultados.insertRow();
+              fila.insertCell(0).innerText = token.value;
+              fila.insertCell(1).innerText = token.type;
+              fila.insertCell(2).innerText = token.line;
+              fila.insertCell(3).innerText = token.column;
+          });
+      })
+      .catch(error => {
+          console.error('Error:', error);
+      });   
   });
